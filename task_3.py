80eda584c88d14739f28d8e316e9f5f6423b69d4
JuliaPavlova <julianamur555@yandex.ru>
2025-05-26 18:16:30 +0300

    new version the final project is 2 sprints, task_3

--- task_3.py
+++ task_3.py
@@ -1,42 +1,37 @@
 class PointsForPlace:
-    points = 0
-    
-    @classmethod
-    def get_points_for_place(cls, place):
+    def __init__(self):
+        self.points = 0
+
+    @staticmethod
+    def get_points_for_place(place):
         if place > 100:
             print('Баллы начисляются только первым 100 участникам')
-            cls.points = 0
         elif place < 1:
             print('Спортсмен не может занять нулевое или отрицательное место')
-            cls.points = 0
         else:
-            cls.points = 101 - place
-        return cls.points
-
+            return 101 - place
 
 class PointsForMeters:
-    points = 0
-    
-    @classmethod
-    def get_points_for_meters(cls, meters):
+    def __init__(self):
+        self.points = 0
+
+    @staticmethod
+    def get_points_for_meters(meters):
         if meters < 0:
             print('Количество метров не может быть отрицательным')
-            cls.points = 0
         else:
-            cls.points = meters * 0.5
-        return cls.points
-
+            return meters * 0.5
 
 class TotalPoints(PointsForPlace, PointsForMeters):
-    total = 0
-    
-    @classmethod
-    def get_total_points(cls, meters, place):
-        place_points = cls.get_points_for_place(place)
-        meters_points = cls.get_points_for_meters(meters)
-        cls.total = place_points + meters_points
-        return cls.total
+    def __init__(self):
+        super().__init__()
+        self.total = 0
 
+    def get_total_points(self, meters, place):
+        place_points = self.get_points_for_place(place)
+        meters_points = self.get_points_for_meters(meters)
+        self.total = place_points + meters_points
+        return self.total
 
 points_for_place = PointsForPlace()
 print(points_for_place.get_points_for_place(10))
@@ -47,4 +42,4 @@ print(points_for_meters.get_points_for_meters(10))
 total_points = TotalPoints()
 print(total_points.get_points_for_place(10))
 print(total_points.get_points_for_meters(10))
-print(total_points.get_total_points(100, 10))
+print(total_points.get_total_points(100, 10)) 
