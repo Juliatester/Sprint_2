c3cb9c11c8e10ab81f3e9ebd443c9aac406d158d
JuliaPavlova <julianamur555@yandex.ru>
2025-05-31 11:20:33 +0300

    new version task_3 31.05

--- task_3.py
+++ task_3.py
@@ -27,7 +27,7 @@ class TotalPoints(PointsForPlace, PointsForMeters):
         return total_points
 
 points_for_place = PointsForPlace()
-print(points_for_place.get_points_for_place(10))
+print(points_for_place.get_points_for_place(10)) 
 
 points_for_meters = PointsForMeters()
 print(points_for_meters.get_points_for_meters(10))
@@ -35,4 +35,4 @@ print(points_for_meters.get_points_for_meters(10))
 total_points = TotalPoints()
 print(total_points.get_points_for_place(10))
 print(total_points.get_points_for_meters(10))
-print(total_points.get_total_points(100, 10)) 
+print(total_points.get_total_points(100, 10))
